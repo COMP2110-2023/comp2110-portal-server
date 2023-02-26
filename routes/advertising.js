3d7bd4a6aeddbe841debe9232b99fcfbd47e2e80
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-02-26 21:38:09 +1100

    Add more ad images and update tracking
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- routes/advertising.js
+++ routes/advertising.js
@@ -3,26 +3,32 @@
  * and the pages they have visited in doing so
  * 
  */
-const { randomInt } = require('crypto');
+const {randomInt} = require('crypto');
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (request, response) => {
     if (request.headers.referer) {
+        const referer = request.headers.referer;
         if (request.session.sites) {
-            request.session.sites.push(request.headers.referer);
+            const entry = request.session.sites;
+            if (referer in entry) {
+                entry[referer] += 1;
+            } else {
+                entry[referer] = 1;
+            }
+            request.session.sites = entry;
         } else {
-            request.session.sites = [request.headers.referer];
+            request.session.sites = {};
+            request.session.sites[referer] = 1;
         }
     }
     // select an image
-    const images = ['Slide1.png', 'Slide2.png']
-
-    const choice = images[randomInt(images.length)]
-    
+    const images = fs.readdirSync('public/images/ad-images/');
+    const choice = images[randomInt(images.length)];    
     response.sendFile(`public/images/ad-images/${choice}`,
-                       {root: '.'})
+                       {root: '.'});
 })
 
 
