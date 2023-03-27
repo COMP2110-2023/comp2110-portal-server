f09084ef2862f251f2b9a64f7b2b692681f72e9b
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-03-27 17:10:30 +1100

    don't mention mongodb
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- config.js
+++ config.js
@@ -5,9 +5,8 @@ if (process.env.NODE_ENV != 'production') {
 }
 
 const corsClientDomain =  process.env.CORS_CLIENT_DOMAIN
-const mongoDBUrl = process.env.MONGODB_URL
 const sessionSecret =  process.env.SESSION_DB_SECRET || 'notVerySecretSecret'
 const saltRounds = 10;
 const port = process.env.PORT || '8000'
 
-module.exports = { corsClientDomain, sessionSecret, port, mongoDBUrl, saltRounds}
+module.exports = { corsClientDomain, sessionSecret, port, saltRounds}
